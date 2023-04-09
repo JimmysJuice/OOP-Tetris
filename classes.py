5a9f98fcbaea5e41af55890c409c0f87b7c84dd2
James Corey Rundell <coreycraft123@gmail.com>
2023-04-09 13:48:36 -0400

    finished gameplay mechanics
    Still need to add next piece preview window and scoring system that increases drop speed and keeps track of high scores

--- classes.py
+++ classes.py
@@ -14,6 +14,9 @@ class Block:
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, (self.x * constants.TILE_SIZE, self.y * constants.TILE_SIZE,
                                                constants.TILE_SIZE, constants.TILE_SIZE))
+        pygame.draw.rect(surface, constants.COLORS["black"], (self.x * constants.TILE_SIZE,
+                                                              self.y * constants.TILE_SIZE, constants.TILE_SIZE,
+                                                              constants.TILE_SIZE), 1)
 
     def drop(self):
         self.y += 1
@@ -28,8 +31,8 @@ class Block:
 # Piece represents a piece that is being controlled by the user and not part of the tetris board yet
 class Piece:
     def __init__(self):
-        self.color = random.choice(list(constants.COLORS.values()))
         self.piece_type = random.randrange(0, 7)  # picks random piece type
+        self.color = list(constants.COLORS.values())[self.piece_type]
 
         # determines piece starting position base on piece type
         if self.piece_type == 0:
@@ -96,56 +99,162 @@ class Board:
         for column in range(constants.PLAYFIELD_WIDTH):
             self.state.append([])
             for row in range(constants.PLAYFIELD_HEIGHT):
-                self.state[column].append(" ")  # " " represents and empty tile on the board
-        print("test")
+                self.state[column].append(0)  # 0 represents and empty tile on the board
 
-    # loop through state and draw block when not " "
-    def draw(self):
-        pass
+    # draw all blocks in board state
+    def draw(self, surface):
+        for column in self.state:
+            for row in column:
+                if row != 0:
+                    row.draw(surface)
+
+    # has_block(x, y) checks if Block.state[x][y] has a block
+    def has_block(self, x, y):
+        if 0 <= x < constants.PLAYFIELD_WIDTH and 0 <= y < constants.PLAYFIELD_HEIGHT:
+            if self.state[x][y] != 0:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    # add piece to board state
+    def add_piece(self, piece):
+        for block in piece.blocks:
+            self.state[block.x][block.y] = block
+
+    # detects if board state has a line to clear and returns the row number or -1 if no row is detected
+    def detect_line(self):
+        for row in range(0, constants.PLAYFIELD_HEIGHT):
+            count = 0
+            for column in range(constants.PLAYFIELD_WIDTH):
+                if self.state[column][row] != 0:
+                    count += 1
+            if count == constants.PLAYFIELD_WIDTH:
+                return row
+        return -1
+
+    def clear_line(self, row):
+        for y in range(row, 0, -1):
+            for x in range(0, constants.PLAYFIELD_WIDTH):
+                if self.state[x][y-1] != 0:
+                    self.state[x][y-1].drop()
+                self.state[x][y] = self.state[x][y-1]
+                self.state[x][y-1] = 0
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode(constants.PLAYFIELD_RES)
+        self.playfield = pygame.display.set_mode(constants.PLAYFIELD_RES)
         self.clock = pygame.time.Clock()
         self.is_game_over = False
+        self.board = Board()
         self.piece = Piece()
+        self.clock = pygame.time.Clock()
+        self.time_since_last_drop = 0
+        self.drop_time = 2000  # milliseconds
 
+    # is_valid_move() checks if move is valid and returns true or false accordingly
     def is_valid_move(self, move):
         if move == "down":
-            return True
+            for block in self.piece.blocks:
+                if block.y + 1 >= constants.PLAYFIELD_HEIGHT or self.board.has_block(block.x, block.y + 1):
+                    return False
+            else:
+                return True
         if move == "left":
-            return True
+            for block in self.piece.blocks:
+                if block.x - 1 < 0 or self.board.has_block(block.x - 1, block.y):
+                    return False
+            else:
+                return True
         if move == "right":
-            return True
+            for block in self.piece.blocks:
+                if block.x + 1 >= constants.PLAYFIELD_WIDTH or self.board.has_block(block.x + 1, block.y):
+                    return False
+            else:
+                return True
         if move == "clockwise":
+            self.piece.rotate_clockwise()
+            for block in self.piece.blocks:
+                if block.x >= constants.PLAYFIELD_WIDTH or block.x < 0 or block.y >= constants.PLAYFIELD_HEIGHT or \
+                        self.board.has_block(block.x, block.y):
+                    self.piece.rotate_counterclockwise()
+                    return False
+            self.piece.rotate_counterclockwise()
             return True
+
         if move == "counterclockwise":
+            self.piece.rotate_counterclockwise()
+            for block in self.piece.blocks:
+                if block.x >= constants.PLAYFIELD_WIDTH or block.x < 0 or block.y >= constants.PLAYFIELD_HEIGHT or \
+                        self.board.has_block(block.x, block.y):
+                    self.piece.rotate_clockwise()
+                    return False
+            self.piece.rotate_clockwise()
             return True
+
+    def get_new_piece(self):
+        self.board.add_piece(self.piece)
+        lines = 0
+        while self.board.detect_line() != -1 and lines < 4:
+            self.board.clear_line(self.board.detect_line())
+            lines += 1
+        self.piece = Piece()
+        self.detect_game_over()
+        self.time_since_last_drop = 0
+
+    # get_user_input() takes in user input and moves piece accordingly
     def get_user_input(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN and self.is_valid_move("down"):
-                self.piece.drop()
+            if event.key == pygame.K_DOWN:
+                if self.is_valid_move("down"):
+                    self.piece.drop()
+                    self.time_since_last_drop = 0
+                else:
+                    self.get_new_piece()
             if event.key == pygame.K_LEFT and self.is_valid_move("left"):
                 self.piece.shift_left()
             if event.key == pygame.K_RIGHT and self.is_valid_move("right"):
                 self.piece.shift_right()
-            if event.key == pygame.K_UP and self.is_valid_move("clockwise"):
-                self.piece.drop()
+            if event.key == pygame.K_UP:
+                while self.is_valid_move("down"):
+                    self.piece.drop()
+                self.get_new_piece()
             if event.key == pygame.K_z and self.is_valid_move("counterclockwise"):
-                print("z was pressed!")
                 self.piece.rotate_counterclockwise()
-            if event.key == pygame.K_x:
-                print("x was pressed!")
+            if event.key == pygame.K_x and self.is_valid_move("clockwise"):
                 self.piece.rotate_clockwise()
+
+    # drop_timer() tracks milliseconds since last piece drop and drops again when time reaches drop_time
+    def drop_timer(self):
+        dt = self.clock.tick()
+        self.time_since_last_drop += dt
+        if self.time_since_last_drop >= self.drop_time:
+            if self.is_valid_move("down"):
+                self.piece.drop()
+            else:
+                self.get_new_piece()
+            self.time_since_last_drop = 0
+
+    def detect_game_over(self):
+        for block in self.piece.blocks:
+            if self.board.has_block(block.x, block.y):
+                self.is_game_over = True
+            else:
+                self.is_game_over = False
+
     def run(self):
         while not self.is_game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 self.get_user_input(event)
-
-            self.screen.fill("black")
-            self.piece.draw(self.screen)
+            self.drop_timer()
+            self.playfield.fill("black")
+            self.board.draw(self.playfield)
+            self.piece.draw(self.playfield)
             pygame.display.flip()
+
+        print("Game Over!")
--- constants.py
+++ constants.py
@@ -4,7 +4,6 @@ PLAYFIELD_HEIGHT = 20
 PLAYFIELD_RES = [PLAYFIELD_WIDTH * TILE_SIZE, PLAYFIELD_HEIGHT * TILE_SIZE]
 FPS = 60
 COLORS = {
-    "white": (255, 255, 255),
     "red":  (255, 0, 0),
     "green": (0, 255, 0),
     "blue": (0, 0, 255),
@@ -14,9 +13,11 @@ COLORS = {
     "orange": (255, 165, 0),
     "purple": (128, 0, 128),
     "gray": (128, 128, 128),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
 }
 # PIECES represents the block coordinates for every piece and every possible piece rotation position
-# PIECES[piece type][piece orientation][block coordinates][x or y]
+# PIECES[piece type][piece orientation][block number][block co-ords (x or y)]
 PIECES = ((((1, 3), (2, 3), (3, 3), (4, 3)),  # I piece
            ((3, 1), (3, 2), (3, 3), (3, 4)),
            ((1, 3), (2, 3), (3, 3), (4, 3)),
@@ -31,16 +32,16 @@ PIECES = ((((1, 3), (2, 3), (3, 3), (4, 3)),  # I piece
            ((2, 1), (2, 2), (2, 3), (3, 3))),
           (((1, 2), (2, 2), (3, 2), (3, 3)),  # J piece
            ((2, 1), (2, 2), (1, 3), (2, 3)),
-           ((1, 2), (1, 3), (2, 3), (3, 3)),
+           ((1, 1), (1, 2), (2, 2), (3, 2)),
            ((2, 1), (3, 1), (2, 2), (2, 3))),
           (((2, 2), (3, 2), (1, 3), (2, 3)),  # S piece
            ((2, 1), (2, 2), (3, 2), (3, 3)),
            ((2, 2), (3, 2), (1, 3), (2, 3)),
-           ((1, 1), (1, 2), (2, 2), (2, 3))),
+           ((2, 1), (2, 2), (3, 2), (3, 3))),
           (((1, 2), (2, 2), (2, 3), (3, 3)),  # Z piece
            ((3, 1), (2, 2), (3, 2), (2, 3)),
            ((1, 2), (2, 2), (2, 3), (3, 3)),
-           ((2, 1), (1, 2), (2, 2), (1, 3))),
+           ((3, 1), (2, 2), (3, 2), (2, 3))),
           (((2, 2), (3, 2), (2, 3), (3, 3)),  # O piece
            ((2, 2), (3, 2), (2, 3), (3, 3)),
            ((2, 2), (3, 2), (2, 3), (3, 3)),
