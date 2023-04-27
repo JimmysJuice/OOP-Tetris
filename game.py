d1bcf54709e065627dba8d33d00812c3fbb42176
James Corey Rundell <coreycraft123@gmail.com>
2023-04-27 13:13:05 -0400

    updated scoring and leveling system

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.font.init()
-        self.my_font = pygame.font.SysFont('Arial', 30)
+        self.my_font = pygame.font.SysFont('Arial', 18)
         self.display = pygame.display.set_mode(constants.DISPLAY_RES)
         self.playfield = pygame.Surface(constants.PLAYFIELD_RES)
         self.info_panel = pygame.Surface(constants.INFO_PANEL_RES)
@@ -21,6 +21,7 @@ class Game:
         self.clock = pygame.time.Clock()
         self.time_since_last_drop = 0
         self.drop_time = 1500  # milliseconds
+        self.lines = 0
         self.score = 0
 
     # check if move is valid and returns true or false accordingly
@@ -93,14 +94,31 @@ class Game:
         lines = 0
         while self.board.detect_line() != -1 and lines < 4:
             self.board.clear_line(self.board.detect_line())
-            self.score += 1
+            self.lines += 1
             lines += 1
-        self.score += lines
-        if self.get_level() > initial_level:
-            self.decrease_drop_time()
+        if lines > 0:
+            self.update_score(lines)
+            if self.get_level() > initial_level:
+                self.decrease_drop_time()
+
+    def update_score(self, lines):
+        points_to_add = 0
+        if lines == 1:
+            points_to_add = 40
+        elif lines == 2:
+            points_to_add = 100
+        elif lines == 3:
+            points_to_add = 300
+        elif lines == 4:
+            points_to_add = 1200
+
+        # add 10% increase to points_to_add per player level
+        points_to_add = int(points_to_add + ((self.get_level() * 0.1) * points_to_add))
+
+        self.score += points_to_add
 
     def get_level(self):
-        return self.score // 4
+        return self.lines // 10
 
     def decrease_drop_time(self):
         self.drop_time *= 0.8
@@ -146,18 +164,20 @@ class Game:
             else:
                 self.is_game_over = False
 
-    # draws all surfaces on pygame window
+    # draws all surfaces and text on pygame window
     def draw_displays(self):
         self.playfield.fill("black")
         self.info_panel.fill("gray19")
         self.board.draw(self.playfield)
         self.piece.draw(self.playfield)
         next_piece_text_surface = self.my_font.render('Next:', False, constants.COLORS["white"])
-        self.info_panel.blit(next_piece_text_surface, (30, 10))
+        self.info_panel.blit(next_piece_text_surface, (50, 10))
         level_text = self.my_font.render(f'Level: {self.get_level()}', False, constants.COLORS["white"])
-        self.info_panel.blit(level_text, (25, 450))
+        self.info_panel.blit(level_text, (10, 450))
+        lines_text = self.my_font.render(f'Score: {self.lines}', False, constants.COLORS["white"])
+        self.info_panel.blit(lines_text, (10, 500))
         score_text = self.my_font.render(f'Score: {self.score}', False, constants.COLORS["white"])
-        self.info_panel.blit(score_text, (20, 500))
+        self.info_panel.blit(score_text, (10, 550))
         self.piece.draw_next_piece(self.info_panel)
         self.display.blit(self.playfield, (0, 0))
         self.display.blit(self.info_panel, (constants.PLAYFIELD_WIDTH * constants.BLOCK_SIZE, 0))
@@ -173,3 +193,6 @@ class Game:
             self.drop_timer()
             self.draw_displays()
         print("Game Over!")
+        print(f"Level: {self.get_level()}")
+        print(f"Lines: {self.lines}")
+        print(f"Score: {self.score}")
